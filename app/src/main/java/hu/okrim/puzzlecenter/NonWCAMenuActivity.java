a6631d77ac06dc2dafdc770f633296a5d4296dca
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-02-03 15:05:30 +0100

    Created an activity for all supported puzzles.

--- app/src/main/java/hu/okrim/puzzlecenter/NonWCAMenuActivity.java
+++ app/src/main/java/hu/okrim/puzzlecenter/NonWCAMenuActivity.java
@@ -2,13 +2,59 @@ package hu.okrim.puzzlecenter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class NonWCAMenuActivity extends AppCompatActivity {
+
+    Button buttonVoid;
+    Button buttonSuperIvy;
+    Button buttonPentacle;
+    Button button2x2Pyraminx;
+    Button button2x2x4;
+    Button buttonMasterPyraminx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_non_wcamenu);
+
+        buttonVoid = findViewById(R.id.buttonVoid);
+        buttonSuperIvy = findViewById(R.id.buttonSuperIvy);
+        buttonPentacle = findViewById(R.id.buttonPentacle);
+        button2x2Pyraminx = findViewById(R.id.button2x2Pyraminx);
+        button2x2x4 = findViewById(R.id.button2x2x4);
+        buttonMasterPyraminx = findViewById(R.id.buttonMasterPyraminx);
+    }
+
+    public void loadNewIntent(android.view.View source){
+        Class classToLoad = null;
+        Intent intent;
+
+        if(source == buttonVoid){
+            classToLoad = PuzzleVoidActivity.class;
+        }
+        else if(source == buttonSuperIvy){
+            classToLoad = PuzzleSuperIvyActivity.class;
+        }
+        else if(source == buttonPentacle){
+            classToLoad = PuzzlePentacleActivity.class;
+        }
+        else if(source == button2x2Pyraminx){
+            classToLoad = Puzzle2x2PyraminxActivity.class;
+        }
+        else if(source == buttonMasterPyraminx){
+            classToLoad = PuzzleMasterPyraminxActivity.class;
+        }
+
+        //Defining which class to load in the new intent
+        intent = new Intent(this, classToLoad);
+        try{
+            startActivity(intent);
+        }catch(NullPointerException NPE){
+            Log.d("ActivityError", NPE.getMessage());
+        }
     }
 }
--- app/src/main/java/hu/okrim/puzzlecenter/WCAMenuActivity.java
+++ app/src/main/java/hu/okrim/puzzlecenter/WCAMenuActivity.java
@@ -2,13 +2,57 @@ package hu.okrim.puzzlecenter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class WCAMenuActivity extends AppCompatActivity {
+
+    Button buttonSkewb;
+    Button buttonSquareOne;
+    Button buttonPyraminx;
+    Button buttonMegaminx;
+    Button buttonClock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wcamenu);
+
+        buttonSkewb = findViewById(R.id.buttonSkewb);
+        buttonSquareOne = findViewById(R.id.buttonSquareOne);
+        buttonPyraminx = findViewById(R.id.buttonPyraminx);
+        buttonMegaminx = findViewById(R.id.buttonMegaminx);
+        buttonClock = findViewById(R.id.buttonClock);
+    }
+
+    public void loadNewIntent(android.view.View source){
+        Class classToLoad = null;
+        Intent intent;
+
+        if(source == buttonSkewb){
+            classToLoad = PuzzleSkewbActivity.class;
+        }
+        else if(source == buttonSquareOne){
+            classToLoad = PuzzleSquareOneActivity.class;
+        }
+        else if(source == buttonPyraminx){
+            classToLoad = PuzzlePyraminxActivity.class;
+        }
+        else if(source == buttonMegaminx){
+            classToLoad = PuzzleMegaminxActivity.class;
+        }
+        else if(source == buttonClock){
+            classToLoad = PuzzleMagicClockActivity.class;
+        }
+
+        //Defining which class to load in the new intent
+        intent = new Intent(this, classToLoad);
+        try{
+            startActivity(intent);
+        }catch(NullPointerException NPE){
+            Log.d("ActivityError", NPE.getMessage());
+        }
     }
 }
