0e0c3b54f96649300bc0af15bf15dc2d044dca2a
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-02-11 16:15:18 +0100

    Added an additional step to the 5x5 tutorial

--- app/src/main/java/hu/okrim/puzzlecenter/Puzzle5x5x5Activity.java
+++ app/src/main/java/hu/okrim/puzzlecenter/Puzzle5x5x5Activity.java
@@ -11,6 +11,7 @@ public class Puzzle5x5x5Activity extends AppCompatActivity {
     CardView cardViewStep1;
     CardView cardViewStep2;
     CardView cardViewStep3;
+    CardView cardViewStep4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +21,18 @@ public class Puzzle5x5x5Activity extends AppCompatActivity {
         cardViewStep1 = findViewById(R.id.cVStep1);
         cardViewStep2 = findViewById(R.id.cVStep2);
         cardViewStep3 = findViewById(R.id.cVStep3);
+        cardViewStep4 = findViewById(R.id.cVStep4);
 
         if(getIntent().getExtras().getBoolean("onlyAlgs")){
             cardViewStep1.setVisibility(View.GONE);
             cardViewStep2.setVisibility(View.GONE);
             cardViewStep3.setVisibility(View.GONE);
+            cardViewStep4.setVisibility(View.GONE);
         }else{
             cardViewStep1.setVisibility(View.VISIBLE);
             cardViewStep2.setVisibility(View.VISIBLE);
             cardViewStep3.setVisibility(View.VISIBLE);
+            cardViewStep4.setVisibility(View.VISIBLE);
         }
     }
 }
