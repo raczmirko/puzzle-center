eb8cc4a5be2cc640136e765b77f611158fed9551
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-02-11 10:50:11 +0100

    Started the 6x6x6 puzzle page but postponed it until further testing with a physical cube (I don't own one currently)

--- app/src/main/java/hu/okrim/puzzlecenter/Puzzle6x6x6Activity.java
+++ app/src/main/java/hu/okrim/puzzlecenter/Puzzle6x6x6Activity.java
@@ -1,14 +1,42 @@
 package hu.okrim.puzzlecenter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Puzzle6x6x6Activity extends AppCompatActivity {
+
+    CardView cardViewStep0;
+    CardView cardViewStep1;
+    CardView cardViewStep2;
+    CardView cardViewStep3;
+    CardView cardViewStep4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_6x6x6);
+
+        cardViewStep0 = findViewById(R.id.cVStep0);
+        cardViewStep1 = findViewById(R.id.cVStep1);
+        cardViewStep2 = findViewById(R.id.cVStep2);
+        cardViewStep3 = findViewById(R.id.cVStep3);
+        cardViewStep4 = findViewById(R.id.cVStep4);
+
+        if(getIntent().getExtras().getBoolean("onlyAlgs")){
+            cardViewStep0.setVisibility(View.GONE);
+            cardViewStep1.setVisibility(View.GONE);
+            cardViewStep2.setVisibility(View.GONE);
+            cardViewStep3.setVisibility(View.GONE);
+            cardViewStep4.setVisibility(View.GONE);
+        }else{
+            cardViewStep0.setVisibility(View.VISIBLE);
+            cardViewStep1.setVisibility(View.VISIBLE);
+            cardViewStep2.setVisibility(View.VISIBLE);
+            cardViewStep3.setVisibility(View.VISIBLE);
+            cardViewStep4.setVisibility(View.VISIBLE);
+        }
     }
 }
