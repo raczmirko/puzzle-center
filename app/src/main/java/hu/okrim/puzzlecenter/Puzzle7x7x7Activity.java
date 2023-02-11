e0c0839202d41e2254fecd083f67725b21f727aa
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-02-11 14:02:38 +0100

    7x7 puzzle page added

--- app/src/main/java/hu/okrim/puzzlecenter/Puzzle7x7x7Activity.java
+++ app/src/main/java/hu/okrim/puzzlecenter/Puzzle7x7x7Activity.java
@@ -1,14 +1,34 @@
 package hu.okrim.puzzlecenter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Puzzle7x7x7Activity extends AppCompatActivity {
+
+    CardView cardViewStep1;
+    CardView cardViewStep2;
+    CardView cardViewStep3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_7x7x7);
+
+        cardViewStep1 = findViewById(R.id.cVStep1);
+        cardViewStep2 = findViewById(R.id.cVStep2);
+        cardViewStep3 = findViewById(R.id.cVStep3);
+
+        if(getIntent().getExtras().getBoolean("onlyAlgs")){
+            cardViewStep1.setVisibility(View.GONE);
+            cardViewStep2.setVisibility(View.GONE);
+            cardViewStep3.setVisibility(View.GONE);
+        }else{
+            cardViewStep1.setVisibility(View.VISIBLE);
+            cardViewStep2.setVisibility(View.VISIBLE);
+            cardViewStep3.setVisibility(View.VISIBLE);
+        }
     }
 }
