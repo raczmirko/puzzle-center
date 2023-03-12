47becad85a904800bd3f59d611df0104ae056213
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-03-12 19:20:02 +0100

    Completed the Statistics page

--- app/src/main/java/hu/okrim/puzzlecenter/DatabaseController.java
+++ app/src/main/java/hu/okrim/puzzlecenter/DatabaseController.java
@@ -168,8 +168,8 @@ public class DatabaseController extends SQLiteOpenHelper {
                 String puzzle = cursor.getString(2);
                 int time = cursor.getInt(3);
 
-                DataEntryModel newCustomer = new DataEntryModel(dateString, puzzle, time);
-                returnList.add(newCustomer);
+                DataEntryModel newDataEntryModel = new DataEntryModel(dateString, puzzle, time);
+                returnList.add(newDataEntryModel);
 
             }while(cursor.moveToNext());
         }
@@ -178,6 +178,25 @@ public class DatabaseController extends SQLiteOpenHelper {
         cursor.close();
         db.close();
         return returnList;
+    }
+
+    public String getDaysSinceFirstSolve(){
+        //Get data from the Database.
+        String queryString = "WITH X AS (SELECT SUBSTR(MIN(COLUMN_DATE), 1, 10) AS minDate FROM RECORDS_TABLE) SELECT julianday('now') - julianday(date(minDate)) FROM X";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(queryString, null);
+        String returnString = "";
+        //If first element was found, meaning there were results...
+        if(cursor.moveToFirst()){
+            //loop through cursor (result set) and create new customer objects, put them in returnList
+            do{
+                returnString = String.valueOf(cursor.getInt(0));
+            }while(cursor.moveToNext());
+        }
+        //close both cursor and connection
+        cursor.close();
+        db.close();
+        return returnString;
     }
 
     public List<String> getFavouritePuzzle(){
@@ -197,6 +216,104 @@ public class DatabaseController extends SQLiteOpenHelper {
                 returnList.add(times);
             }while(cursor.moveToNext());
         }
+        //close both cursor and connection
+        cursor.close();
+        db.close();
+        return returnList;
+    }
+
+    public String getDistinctDaysOfUse(){
+        //Get data from the Database.
+        String queryString = "SELECT COUNT(DISTINCT SUBSTR(COLUMN_DATE,1,10)) FROM RECORDS_TABLE";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(queryString, null);
+        String returnString = "";
+        //If first element was found, meaning there were results...
+        if(cursor.moveToFirst()){
+            //loop through cursor (result set) and create new customer objects, put them in returnList
+            do{
+                returnString = String.valueOf(cursor.getInt(0));
+            }while(cursor.moveToNext());
+        }
+        //close both cursor and connection
+        cursor.close();
+        db.close();
+        return returnString;
+    }
+
+    public List<String> getStatistics(String puzzle){
+        /*
+        WITH X AS (
+             SELECT * FROM RECORDS_TABLE WHERE COLUMN_PUZZLE LIKE ('%2x2x2%')),
+             Y_PB AS (
+             SELECT MIN(COLUMN_TIME) as pb FROM X),
+             Y_TOTAL AS (
+             SELECT COUNT(1) AS solves FROM X),
+             Y_Ao5_HELP AS (
+             SELECT COLUMN_TIME FROM X ORDER BY COLUMN_DATE DESC LIMIT 5),
+             Y_Ao5 AS (
+             SELECT CAST(AVG(COLUMN_TIME) AS INTEGER) AS ao5 FROM Y_Ao5_HELP WHERE COLUMN_TIME NOT IN (SELECT MIN(COLUMN_TIME) FROM Y_Ao5_HELP UNION SELECT MAX(COLUMN_TIME) FROM Y_Ao5_HELP)),
+             Y_Ao12_HELP AS (
+             SELECT COLUMN_TIME FROM X ORDER BY COLUMN_DATE DESC LIMIT 12),
+             Y_Ao12 AS (
+             SELECT CAST(AVG(COLUMN_TIME) AS INTEGER) AS ao12 FROM Y_Ao12_HELP WHERE COLUMN_TIME NOT IN (SELECT MIN(COLUMN_TIME) FROM Y_Ao12_HELP UNION SELECT MAX(COLUMN_TIME) FROM Y_Ao12_HELP)),
+             Y_FIRST_Ao5_HELP AS(
+             SELECT COLUMN_TIME FROM X ORDER BY COLUMN_DATE ASC LIMIT 5),
+             Y_FIRST_Ao5 AS(
+             SELECT CAST(AVG(COLUMN_TIME) AS INTEGER) AS first_ao5 FROM Y_FIRST_Ao5_HELP WHERE COLUMN_TIME NOT IN (SELECT MIN(COLUMN_TIME) FROM Y_FIRST_Ao5_HELP UNION SELECT MAX(COLUMN_TIME) FROM Y_FIRST_Ao5_HELP)),
+             Y_Ao5_DIFF AS (
+             SELECT first_ao5 - ao5 as diff FROM Y_Ao5, Y_FIRST_Ao5)
+         SELECT pb, solves, ao5, ao12, diff
+         FROM Y_PB, Y_TOTAL, Y_Ao5, Y_Ao12, Y_Ao5_DIFF
+         */
+
+        List<String> returnList = new ArrayList<>();
+        //Get data from the Database.
+        String queryString = "WITH X AS (\n" +
+                "             SELECT * FROM RECORDS_TABLE WHERE COLUMN_PUZZLE LIKE ('%" + puzzle+ "%')),\n" +
+                "             Y_PB AS (\n" +
+                "             SELECT MIN(COLUMN_TIME) as pb FROM X),\n" +
+                "             Y_TOTAL AS (\n" +
+                "             SELECT COUNT(1) AS solves FROM X),\n" +
+                "             Y_Ao5_HELP AS (\n" +
+                "             SELECT COLUMN_TIME FROM X ORDER BY COLUMN_DATE DESC LIMIT 5),\n" +
+                "             Y_Ao5 AS (\n" +
+                "             SELECT CAST(AVG(COLUMN_TIME) AS INTEGER) AS ao5 FROM Y_Ao5_HELP WHERE COLUMN_TIME NOT IN (SELECT MIN(COLUMN_TIME) FROM Y_Ao5_HELP UNION SELECT MAX(COLUMN_TIME) FROM Y_Ao5_HELP)),\n" +
+                "             Y_Ao12_HELP AS (\n" +
+                "             SELECT COLUMN_TIME FROM X ORDER BY COLUMN_DATE DESC LIMIT 12),\n" +
+                "             Y_Ao12 AS (\n" +
+                "             SELECT CAST(AVG(COLUMN_TIME) AS INTEGER) AS ao12 FROM Y_Ao12_HELP WHERE COLUMN_TIME NOT IN (SELECT MIN(COLUMN_TIME) FROM Y_Ao12_HELP UNION SELECT MAX(COLUMN_TIME) FROM Y_Ao12_HELP)),\n" +
+                "             Y_FIRST_Ao5_HELP AS(\n" +
+                "             SELECT COLUMN_TIME FROM X ORDER BY COLUMN_DATE ASC LIMIT 5),\n" +
+                "             Y_FIRST_Ao5 AS(\n" +
+                "             SELECT CAST(AVG(COLUMN_TIME) AS INTEGER) AS first_ao5 FROM Y_FIRST_Ao5_HELP WHERE COLUMN_TIME NOT IN (SELECT MIN(COLUMN_TIME) FROM Y_FIRST_Ao5_HELP UNION SELECT MAX(COLUMN_TIME) FROM Y_FIRST_Ao5_HELP)),\n" +
+                "             Y_Ao5_DIFF AS (\n" +
+                "             SELECT first_ao5 - ao5 as diff FROM Y_Ao5, Y_FIRST_Ao5)\n" +
+                "         SELECT pb, solves, ao5, ao12, diff\n" +
+                "         FROM Y_PB, Y_TOTAL, Y_Ao5, Y_Ao12, Y_Ao5_DIFF";
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        //If first element was found, meaning there were results...
+        if(cursor.moveToFirst()){
+            //loop through cursor (result set) and create new customer objects, put them in returnList
+            do{
+                String pb = cursor.getString(0);
+                String totalSolves = cursor.getString(1);
+                String ao5 = cursor.getString(2);
+                String ao12 = cursor.getString(3);
+                String ao5diff = cursor.getString(4);
+
+                returnList.add(pb);
+                returnList.add(totalSolves);
+                returnList.add(ao5);
+                returnList.add(ao12);
+                returnList.add(ao5diff);
+
+            }while(cursor.moveToNext());
+        }
+
         //close both cursor and connection
         cursor.close();
         db.close();
--- app/src/main/java/hu/okrim/puzzlecenter/Statistics.java
+++ app/src/main/java/hu/okrim/puzzlecenter/Statistics.java
@@ -2,22 +2,40 @@ package hu.okrim.puzzlecenter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class Statistics extends AppCompatActivity {
 
+    String currentPuzzle = null;
+
     TextView tv_totalSolves;
     TextView tv_favouritePuzzle;
     TextView tv_totalSolvesForFavouritePuzzle;
     TextView tv_firstSolveDate;
     TextView tv_firstSolvePuzzle;
+    TextView tv_distinctDaysOfUsage;
+    TextView tv_pb;
+    TextView tv_totalSolveOfSelected;
+    TextView tv_ao5;
+    TextView tv_ao12;
+    TextView tv_ao5diff;
+
     DatabaseController databaseController = new DatabaseController(Statistics.this);
     ImageView iv_FavouritePuzzle;
     ImageView iv_FirstSolve;
+
+    Spinner puzzleTypeSpinner;
+    ArrayAdapter<CharSequence> spinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +48,45 @@ public class Statistics extends AppCompatActivity {
         tv_totalSolvesForFavouritePuzzle = findViewById(R.id.tv_totalSolvesForFavouritePuzzle);
         tv_firstSolveDate = findViewById(R.id.tv_firstSolveDate);
         tv_firstSolvePuzzle = findViewById(R.id.tv_firstSolvePuzzle);
+        tv_distinctDaysOfUsage = findViewById(R.id.tv_distinctDaysOfAppUsageValue);
+        tv_pb = findViewById(R.id.tv_pbValue);
+        tv_totalSolveOfSelected = findViewById(R.id.tv_totalSolvesForSelectedValue);
+        tv_ao5 = findViewById(R.id.tv_ao5Value);
+        tv_ao12 = findViewById(R.id.tv_ao12Value);
+        tv_ao5diff = findViewById(R.id.tv_ao5DiffValue);
+
 
         iv_FavouritePuzzle = findViewById(R.id.iv_favouritePuzzle);
         iv_FirstSolve = findViewById(R.id.iv_firstSolve);
 
+        puzzleTypeSpinner = findViewById(R.id.spinner);
+
+        spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.spinnerPuzzleType, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(R.layout.custom_spinner_layout);
+        puzzleTypeSpinner.setAdapter(spinnerAdapter);
+        puzzleTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currentPuzzle = parent.getItemAtPosition(position).toString();
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
+                ((TextView) parent.getChildAt(0)).setTextSize(30);
+                loadSpecificStatistics(currentPuzzle);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        loadLastSelectedPuzzleToSpinner();
         loadGeneralStatistics();
     }
 
     public void loadGeneralStatistics(){
         //Refresh statistics onCreate
-        //1st card
+        //1st card (total solves)
             //This one doesn't need error handling since if there are no records counter returns 0
             tv_totalSolves.setText(databaseController.getTotalSolves());
-        //2nd card
+        //2nd card (most trained puzzle)
             //getFavouritePuzzle() returns a list with 2 columns:
             List<String> favouritePuzzleDetails = databaseController.getFavouritePuzzle();
             //1st is puzzle and 2nd is counter (how many times it was played)
@@ -60,21 +104,70 @@ public class Statistics extends AppCompatActivity {
                 tv_totalSolvesForFavouritePuzzle.setText(R.string.total_solves_default);
                 iv_FavouritePuzzle.setImageResource(R.drawable.questionmark);
             }
-        //3rd card
+        //3rd card (first solve)
             List<DataEntryModel> firstSolveDetails = databaseController.getFirstSolve();
             if(!firstSolveDetails.isEmpty()){
                 String firstSolvePuzzle = firstSolveDetails.get(0).getPuzzle();
-                String firstSolveDate = firstSolveDetails.get(0).getDateString();
+                String firstSolveDate = firstSolveDetails.get(0).getDateString().substring(0,10);
+                firstSolveDate = firstSolveDate.concat(" ~ " + databaseController.getDaysSinceFirstSolve() + " days ago");
                 tv_firstSolvePuzzle.setText(firstSolvePuzzle);
                 tv_firstSolveDate.setText(firstSolveDate);
                 setIconOfPuzzle(firstSolvePuzzle, iv_FirstSolve);
             }
             //Defaults
-        else{
-            tv_firstSolvePuzzle.setText(R.string.none_yet);
-            tv_firstSolveDate.setText(R.string.dash);
-            iv_FirstSolve.setImageResource(R.drawable.questionmark);
+            else{
+                tv_firstSolvePuzzle.setText(R.string.none_yet);
+                tv_firstSolveDate.setText(R.string.dash);
+                iv_FirstSolve.setImageResource(R.drawable.questionmark);
+                }
+        //4th card (number of distinct days the app was opened on)
+            //No need for error handling as COUNT gives 0 if there are no records
+            int distinctDaysValue = Integer.parseInt(databaseController.getDistinctDaysOfUse());
+            String distinctDaysText;
+            if(distinctDaysValue == 1) {
+                distinctDaysText = "on " + databaseController.getDistinctDaysOfUse() + " day.";
             }
+            else{
+                distinctDaysText = "on " + databaseController.getDistinctDaysOfUse() + " different days.";
+            }
+            tv_distinctDaysOfUsage.setText(distinctDaysText);
+
+    }
+
+    public void loadSpecificStatistics(String puzzle){
+        List<String> statisticsList = databaseController.getStatistics(puzzle);
+        if(!statisticsList.isEmpty()){
+            //Setting pb
+            if(!(statisticsList.get(0) == null)){
+                String pbText = TimeFormatController.createTimeText(Integer.parseInt(statisticsList.get(0)));
+                tv_pb.setText(pbText);
+            }
+            //Setting total solves for selected puzzle
+            if(!(statisticsList.get(1) == null)){
+                tv_totalSolveOfSelected.setText(statisticsList.get(1));
+            }
+            //Setting ao5
+            if(!(statisticsList.get(2) == null)){
+                String ao5String = statisticsList.get(2);
+                int ao5Int = Integer.parseInt(ao5String);
+                String ao5StringFormatted = TimeFormatController.createTimeText(ao5Int);
+                tv_ao5.setText(ao5StringFormatted);
+            }
+            //Setting ao12
+            if(!(statisticsList.get(3) == null)){
+                String ao12String = statisticsList.get(3);
+                int ao12Int = Integer.parseInt(ao12String);
+                String ao12StringFormatted = TimeFormatController.createTimeText(ao12Int);
+                tv_ao12.setText(ao12StringFormatted);
+            }
+            //Setting ao5 diff
+            if(!(statisticsList.get(4) == null)){
+                String diffString = statisticsList.get(4);
+                int diffInt = Integer.parseInt(diffString);
+                String diffStringFormatted = TimeFormatController.createTimeText(diffInt);
+                tv_ao5diff.setText(diffStringFormatted);
+            }
+        }
     }
 
     public void setIconOfPuzzle(String puzzle, ImageView icon){
@@ -131,5 +224,34 @@ public class Statistics extends AppCompatActivity {
                 icon.setImageResource(R.drawable.masterpyraminx);
                 break;
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadLastSelectedPuzzleToSpinner();
+    }
+
+    private void loadLastSelectedPuzzleToSpinner() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        //Loading currentPuzzle from SharedPreferences, or if not found then 2x2x2 will be default
+        currentPuzzle = sharedPreferences.getString("puzzleSelected", "2x2x2");
+        //Setting the spinner to the correct puzzle
+        //spinnerAdapter.setDropDownViewResource(spinnerAdapter.getPosition(currentPuzzle));
+        puzzleTypeSpinner.setSelection(spinnerAdapter.getPosition(currentPuzzle));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveSelectedPuzzle();
+    }
+
+    private void saveSelectedPuzzle() {
+        //Saving what puzzle was selected on the spinner.
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("puzzleSelected", currentPuzzle);
+        editor.apply();
     }
 }
